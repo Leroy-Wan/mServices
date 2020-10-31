468e7b6055b69ddaeaadc905e964377dad1bbcde
Leroy_Wan <164495266@qq.com>
2020-10-31 23:07:08 +0800

    request handler 切入点和接口设计

--- manager.py
+++ manager.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
+import json
+
 import tornado.web
 from tornado.httputil import HTTPServerRequest
 from tornado.ioloop import IOLoop
@@ -39,8 +41,14 @@ class IndexHandler(tornado.web.RequestHandler):
 
     def post(self):
         # 新增数据
-        self.get_arguments('name', 'city')
-        self.write('<h3>我是POST请求方式</h3>')
+        # 读取表单参数
+        # name = self.get_argument('name')
+        # city = self.get_argument('city')
+        # self.get_arguments('name', 'city')
+        name = self.get_body_argument('name')
+        city = self.get_body_argument('city')
+
+        self.write('<h3>我是POST请求方式: %s %s </h3>' %(name, city))
 
     def put(self):
         self.write('<h3>我是PUT请求方式</h3>')
@@ -48,10 +56,147 @@ class IndexHandler(tornado.web.RequestHandler):
     def delete(self):
         self.write('<h3>我是DELETE请求方式</h3>')
 
+class SearchHandler(tornado.web.RequestHandler):
+    mapper = {
+        'python':'Python是目前世界最流行的AI语言',
+        'java':'Java已经是20多年企业级应用开发语言',
+        'H5': 'H5全称是HTML5，于2014流行的前端WEB标签语言'
+    }
+    def get(self):
+        html = """
+            <h3>搜索%s结果</h3>
+            <p>
+                %s
+            </p>
+        """
+        wd = self.get_query_argument('wd')
+        result = self.mapper.get(wd)
+
+        # self.write(html % (wd, result))
+        resp_data= {
+            'wd': wd,
+            'result': result
+        }
+        self.write(json.dumps(resp_data))
+        self.set_status(200) # 设置响应状态码
+        # 设置响应头的数据类型
+        self.set_header('Content-Type', 'application/json;charset=utf-8')
+
+        # cookie操作
+        self.set_cookie('wd', wd)
+
+class CookieHandler(tornado.web.RequestHandler):   # http://localhost:9000/cookie
+    def get(self):
+        # 验证参数中是否存在name?
+        if self.request.arguments.get('name'):
+
+            # 从查询参数中读取Cookie的名称
+            name = self.get_query_argument('name')
+
+            # 从cookies中获取name的对象或值
+            value = self.get_cookie(name)
+            print(type(value))
+            self.write(value)
+        else:
+            # 查看所有的cookie
+            cookies: dict = self.request.cookies
+            html = '<ul>%s</ul>'
+            lis = []
+            for key in cookies:
+                lis.append('<li>%s: %s</li>' % (key, self.get_cookie(key)))
+            #self.write('显示所有cookie'+html % ''.join(lis))
+            html = '显示所有cookie'+html % ''.join(lis)
+            html += """
+                <form method="post">
+                <input name="name" placeholder="请输入cookie的名称">
+                <button>提交</button>
+                </form>
+            """
+            self.write(html)
+    #def delete(self):  不能使用delete，系统不认这种方法
+    def post(self):
+        name = self.get_argument('name')
+        if self.request.cookies.get(name, None):
+            # 存在的
+            self.clear_cookie(name)
+            self.write('<h3 style="color:green;">删除 %s 成功</h3>' % name)
+        else:
+            self.write('<h3 style="color:red;">删除 %s 失败， 不存在！</h3>' % name)
+
+        # 重定向操作时，不需要再调用self.write()
+        self.redirect('/cookie')  # 重定向
+
+class OrderHandler(tornado.web.RequestHandler):
+    goods =[
+        {
+            'id': 1,
+            'name': 'Python高级开发',
+            'author': 'leroy',
+            'price': 190
+        },
+        {
+            'id': 2,
+            'name': 'Python3 Vs Python2',
+            'author': 'leroy',
+            'price': 290
+        }
+    ]
+
+    action_map = {
+        1: '取消订单',
+        2: '再次购买',
+        3: '评价'
+    }
+
+    def query(self, id):
+        for item in self.goods:
+            if item.get('id') == id:
+                return item
+
+    def initialize(self):
+        # 所有的请求方法在调用之前，都会进行初始化操作
+        print('--------initialize--------')
+
+    def prepare(self):
+        # 在初始化之后，调用行为方法之前，
+        # 调用此方法进行预处理
+        print('-----prepare------')
+
+    def on_finish(self):
+        print('-----on_finish------')
+
+
+
+    def get(self, id, code):
+        print('-----get-----')
+        self.write('订单查询')
+        html = """
+            <p>
+                商品编号： %s
+            </p>
+            <p>
+                商品名称： %s
+            </p>
+            <p>
+                商品价格： %s
+            </p>
+        """
+        goods = self.query(int(id))
+        self.write(html % (goods.get('id'),goods.get('name'),goods.get('price')))
+        self.write(self.action_map.get(int(code)))
+
+    def post(self, code, id):
+        print('-----post------')
+        self.write('----post----')
+
+
 
 def make_app():
     return tornado.web.Application([
         ('/', IndexHandler),
+        ('/search', SearchHandler),
+        ('/cookie', CookieHandler),
+        (r'/order/(?P<code>\d+)/(?P<id>\d+)', OrderHandler)  # 使用正则传参了,按照分组名称传参
     ], default_host=tornado.options.options.host)
 
 
